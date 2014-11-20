78e97ab4c4a5a0f20a5f8e78ddb514792623bed7
Bill Israel <bill.israel@gmail.com>
2014-11-20 13:40:02 -0600

    Update bob exercise to latest version

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,28 +1,20 @@
-class Bob(object):
+def hey(what):
     """
-    Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-
-    Bob answers 'Sure.' if you ask him a question.
-    He answers 'Woah, chill out!' if you yell at him.
-    He says 'Fine. Be that way!' if you address him without actually saying anything.
-    He answers 'Whatever.' to anything else.
+    Bob is a lackadaisical teenager. In conversation, his responses are limited.
+    Bob answers 'Sure.' if you ask him a question. He answers 'Whoa, chill out!'
+    if you yell at him. He says 'Fine. Be that way!' if you address him without
+    actually saying anything. He answers 'Whatever.' to anything else.
     """
-    def hey(self, msg):
-        """
-        Say something to Bob, he'll respond in turn.
+    what = what.strip()
 
-        :param msg: The message for Bob to respond to.
-        """
-        msg = msg.strip()
+    if what.isupper():
+        return 'Whoa, chill out!'
 
-        if not msg:
-            return 'Fine. Be that way!'
+    if what.endswith('?'):
+        return 'Sure.'
 
-        if msg.isupper():
-            return 'Woah, chill out!'
+    if not what:
+        return 'Fine. Be that way!'
 
-        if msg.endswith('?'):
-            return 'Sure.'
-
-        return 'Whatever.'
+    return 'Whatever.'
 
