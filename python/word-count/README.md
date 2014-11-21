1eb08816018b524f1b45042ef71003e1e1eed990
Bill Israel <bill.israel@gmail.com>
2014-11-21 15:22:37 -0600

    Updated word-count for latest unit tests

--- python/word-count/README.md
+++ python/word-count/README.md
@@ -12,6 +12,11 @@ free: 1
 ```
 
 
+### Submitting Exercises
+
+Note that, when trying to submit an exercise, make sure the solution is in the `exercism/python/<exerciseName>` directory.
+
+For example, if you're submitting `bob.py` for the Bob exercise, the submit command would be something like `exercism submit <path_to_exercism_dir>/python/bob/bob.py`.
 
 ## Source
 
--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,16 +1,6 @@
-import re
-import string
 import collections
 
-class Phrase(object):
-    """
-    A program that given a phrase will count the occurrences of each word
-    in that phrase (excluding punctuation, and ignoring case).
-    """
-    def __init__(self, phrase):
-        punc = re.escape(string.punctuation)
-        self._words = re.sub('[{}]'.format(punc), '', phrase).lower()
-    
-    def word_count(self):
-        return collections.Counter(self._words.split())
+def word_count(phrase):
+    """Given a phrase, count the occurrences of each word in that phrase."""
+    return collections.Counter(phrase.split())
 
