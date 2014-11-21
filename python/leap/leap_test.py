c8383295a8cbfdbdadb3150ca915308eb29d5670
Bill Israel <bill.israel@gmail.com>
2014-11-21 15:22:52 -0600

    Add python solution to leap problem

---
+++ python/leap/leap_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from year import is_leap_year
+
+
+class YearTest(unittest.TestCase):
+    def test_leap_year(self):
+        self.assertIs(is_leap_year(1996), True)
+
+    def test_non_leap_year(self):
+        self.assertIs(is_leap_year(1997), False)
+
+    def test_non_leap_even_year(self):
+        self.assertIs(is_leap_year(1998), False)
+
+    def test_century(self):
+        self.assertIs(is_leap_year(1900), False)
+
+    def test_exceptional_century(self):
+        self.assertIs(is_leap_year(2400), True)
+
+    def test_exceptional_century2(self):
+        self.assertIs(is_leap_year(2000), True)
+
+if __name__ == '__main__':
+    unittest.main()
