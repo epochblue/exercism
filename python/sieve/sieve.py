ff6880a3f2c77428e2b99eb2316fb08167ab1556
Bill Israel <bill.israel@gmail.com>
2015-11-01 15:20:15 -0600

    Iterate on Seieve of Eratosthenes solution

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -3,10 +3,13 @@ def sieve(limit):
     Return a list of prime numbers up to the given limit using the
     Sieve of Eratosthenes algorithm.
     """
-    numbers = list(xrange(2, limit+1))
-    for i in numbers:
-        for a in numbers:
-            if i != a and a % i == 0:
-                numbers.remove(a)
+    primes = []
+    non_primes = set()
 
-    return numbers
+    for i in xrange(2, limit+1):
+        if i not in non_primes:
+            primes.append(i)
+
+            for a in xrange(i*i, limit+1, i):
+                non_primes.add(a)
+    return primes
